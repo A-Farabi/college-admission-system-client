5e1ecc19504581828540561e5163ca9c095c077c
A-Farabi <farabisyl2017@gmail.com>
2024-11-21 18:08:04 +0600

    location description added

--- src/Pages/Location.jsx
+++ src/Pages/Location.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const Location = () => {
   return (
-    <section className="bg-gray-50 py-16">
+    <section className="bg-gray-50 py-16 mt-10">
       <div className="container mx-auto px-6 md:px-12 lg:px-20">
         <h2 className="text-3xl font-bold text-center text-gray-800 mb-8">
           Our Location
         </h2>
+        <p className="text-gray-500 text-lg w-[60%] mx-auto text-center mb-10">We're nestled in the heart of Sylhet, offering a convenient and accessible location for students, visitors, and professionals alike. Discover us at the vibrant hub of Jollarpar Road, Zindabazar—a place where innovation meets community</p>
         <div className="flex flex-col lg:flex-row gap-8 items-center">
           {/* Location Details */}
           <div className="lg:w-1/2 space-y-6">
