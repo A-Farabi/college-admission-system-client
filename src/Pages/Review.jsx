1aea4f7a93a216091a25c9e3ed4d6f8a66c7d54c
A-Farabi <farabisyl2017@gmail.com>
2024-11-21 17:23:51 +0600

    review headline modified

--- src/Pages/Review.jsx
+++ src/Pages/Review.jsx
@@ -13,7 +13,7 @@ const Review = () => {
   return (
     <section className="bg-gray-50 py-16 mt-10">
       <div className="container mx-auto px-6 md:px-12 lg:px-20">
-        <h2 className="text-3xl font-bold text-center text-gray-800 mb-8">
+        <h2 className="text-3xl font-bold text-center text-yellow-500 mb-8">
           What People Say About Us
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
