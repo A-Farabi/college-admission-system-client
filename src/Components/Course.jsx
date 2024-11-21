c0f0f2bcfc2c308bb65c24f5232e03d32854e4ee
A-Farabi <farabisyl2017@gmail.com>
2024-11-21 18:50:02 +0600

    course card modiefied

--- src/Components/Course.jsx
+++ src/Components/Course.jsx
@@ -32,7 +32,7 @@ const Courses = () => {
                   {course.title}
                 </h3>
                 <p className="text-gray-600">{course.description}</p>
-                <button className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300">
+                <button className="mt-4 px-6 py-2 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600 transition duration-300">
                   Learn More
                 </button>
               </div>
