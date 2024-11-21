54c15e7ade39e6f4ed3f03390626fdf04e98818f
A-Farabi <farabisyl2017@gmail.com>
2024-11-21 23:30:28 +0600

    getting login data in clg

--- src/Components/AuthComponent/Login.jsx
+++ src/Components/AuthComponent/Login.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
 const Login = ({ toggleToSignup }) => {
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        const form = e.target
+
+        const email = form.email.value
+        const password = form.password.value
+
+    }
+
     return (
         <div className="max-w-md mx-auto bg-white rounded-lg shadow-md p-8">
             <h2 className="text-2xl font-bold text-center mb-4">Login</h2>
-            <form className="space-y-4">
+            <form onSubmit={handleSubmit} className="space-y-4">
                 <div>
                     <label className="block text-sm font-medium text-gray-700">Email</label>
                     <input
